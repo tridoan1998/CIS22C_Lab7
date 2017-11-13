ecab9d33fc7b98a039b71991c4599e48d737d8aa
Tri Doan <tridoan1998@gmail.com>
2017-11-12 21:36:42 -0800

    Add files via upload

--- HashTable.cpp
+++ HashTable.cpp
@@ -29,11 +29,34 @@ int HashTable::hash(string key) const
 
 int HashTable::countBucket(int index) const
 {
-	Table = new Node[10];
-
 	return 0;
 }
 //counts the number of Books at this index
 //returns the count
 //pre: 0<= index < SIZE
 
+void HashTable::insert(Book b)
+{
+	string key = b.get_title() + b.get_author();
+	int index = hash(key);
+	Table[index].insertStop(b);
+}
+
+void HashTable::remove(Book b)
+{
+
+	string key = b.get_title() + b.get_author();
+	int index = hash(key);
+	if(!Table[index].isEmpty())
+	{
+	Table[index].pointIterator();
+    int position = Table[index].linearSearch(b);
+	Table[index].advanceToIndex(position);
+	Talbe[index].
+	}
+;
+}
+//removes b from the table
+//calls the hash function on the key to determine
+//the correct bucket
+//pre: b is in the table
--- HashTest.cpp
+++ HashTest.cpp
@@ -9,7 +9,8 @@
 #include <iomanip>
 #include <string>
 #include "HashTable.h"
-
+#include "Book.h"
+#include "List.h"
 int main()
 {
 	HashTable hashObj;
@@ -17,7 +18,26 @@ int main()
 	string key;
 	cout << "PLease enter book title and author: ";
 	getline(cin, key);
-	cout << hashObj.hash(key);
-	//countBucket(index);
+	int index = hashObj.hash(key);
+	cout << endl;
+	cout << "**********Test countBucket**********" << endl;
+	hashObj.countBucket(index);
+
+
+	HashTable ht;
+	Book pp("Pride and Prejudice", "Jane Austen", 4.95, 1234567);
+	Book c("Contact", "Carl Sagan", 9.95, 99993339);
+	Book hg("The Hunger Games", "Suzanne Collins", 13.55, 12388888);
+	Book hp("Harry Potter", "J.K. Rowlings", 12.99, 55555678);
+	Book mhc("The Man in the High Castle", "Philip K Dick", 15.95, 95959595);
+	Book bh("Bleak House", "Charles Dickens", 8.00, 473890238);
+
+
+	ht.hash(pp.get_title() + pp.get_author());
+	ht.insert(pp);
+	ht.insert(c);
+	ht.insert(hg);
+	ht.insert(mhc);
+	ht.insert(bh);
 	return 0;
 }
